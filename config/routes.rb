e29bd97d1f9e09610fabeca4b03a6935c2296e4c
Pablo Fernandez <heelhook@littleq.net>
2015-02-05 11:37:04 +0100

    Improve devise's paths
    Create `/login`, `/logout`, `/start’ paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' },
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'start',
+    }
 
   mount Split::Dashboard, :at => 'split'
 
